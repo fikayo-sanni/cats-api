9164cfb253a807c48167af32b36c86425eee02c4
Sanni Oluwafikayo <fikayo.iuris@gmail.com>
2024-04-22 19:07:16 +0100

    feat: add admin fetch  users endpoints

--- src/http/api/v1/users/controllers/users.controller.ts
+++ src/http/api/v1/users/controllers/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, ParseIntPipe, Post, Put, Req, Res, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Put, Query, Req, Res, UseGuards, UsePipes } from "@nestjs/common";
 import { UsersService } from "../services/users.service";
 import { BaseAppController } from "src/http/api/base/base.controller";
 import { Roles } from "src/common/decorators/roles.decorator";
@@ -8,6 +8,9 @@ import { CreateCatDto } from "../../cats/dto/cat.create.dto";
 import { Response } from "express";
 import { AccessTokenGuard } from "src/common/guards/accessToken.guard";
 import { RolesGuard } from "src/common/guards/roles.guard";
+import { PaginationPipe } from "src/common/pipes/pagination.pipe";
+import { IPaginationOptions } from "src/common/interfaces/pagination.interface";
+import { generateMetaResponse } from "src/common/utils/pagination.util";
 
 @Controller('api/v1/users')
 @UseGuards(AccessTokenGuard, RolesGuard)
@@ -23,6 +26,28 @@ export class UsersController extends BaseAppController {
     @Res() res: Response,
   ) {
     const result = await this.usersService.makeAdmin(id);
+
+    return this.getHttpResponse().setAuthDataWithKey('data', result).send(res);
+  }
+
+  @Get()
+  @Roles(['admin'])
+  @UsePipes(new PaginationPipe())
+  async findAll(@Query() filterQuery: IPaginationOptions, @Req() req: IAuthRequest,
+    @Res() res: Response) {
+    const { items, count } = await this.usersService.findAll(filterQuery);
+
+    const meta = generateMetaResponse(count, filterQuery, req);
+
+    return this.getHttpResponse().sendResponseBody(res, { data: items, meta });
+  }
+
+  @Get('/:id')
+  @Roles(['admin'])
+  @UsePipes(new PaginationPipe())
+  async findOne(@Param('id', new ParseIntPipe()) id: number, @Req() req: IAuthRequest,
+    @Res() res: Response) {
+    const result = await this.usersService.findOne(id);
 
     return this.getHttpResponse().setAuthDataWithKey('data', result).send(res);
   }
